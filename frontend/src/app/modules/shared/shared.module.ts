00a8a5d1dccd6b0a992039061f39aea5231f70b9
coma64 <zaton.tristan@gmail.com>
2023-04-22 20:35:59 +0200

    Add loading spinner

--- frontend/src/app/modules/shared/shared.module.ts
+++ frontend/src/app/modules/shared/shared.module.ts
@@ -4,10 +4,21 @@ import { BannerComponent } from './banner/banner.component';
 import { IconsModule } from '../login/icons/icons.module';
 import { TimeUntilPipe } from './relative-time/time-until.pipe';
 import { ToRelativeTimePipe } from './relative-time/to-relative-time.pipe';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
-  declarations: [BannerComponent, TimeUntilPipe, ToRelativeTimePipe],
+  declarations: [
+    BannerComponent,
+    TimeUntilPipe,
+    ToRelativeTimePipe,
+    SpinnerComponent,
+  ],
   imports: [CommonModule, IconsModule],
-  exports: [BannerComponent, ToRelativeTimePipe, TimeUntilPipe],
+  exports: [
+    BannerComponent,
+    ToRelativeTimePipe,
+    TimeUntilPipe,
+    SpinnerComponent,
+  ],
 })
 export class SharedModule {}
