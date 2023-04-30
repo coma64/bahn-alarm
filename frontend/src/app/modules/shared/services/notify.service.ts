73ab6f810d7c50a319aa069664f2f83e476be802
coma64 <zaton.tristan@gmail.com>
2023-04-30 13:49:37 +0200

    Add eslint

--- frontend/src/app/modules/shared/services/notify.service.ts
+++ frontend/src/app/modules/shared/services/notify.service.ts
@@ -6,10 +6,12 @@ import { Observable, of } from 'rxjs';
 })
 export class NotifyService {
   error(message: string): void {
+    // eslint-disable-next-line no-use-before-define
     alert(message);
   }
 
   confirm(message: string): Observable<boolean> {
+    // eslint-disable-next-line no-use-before-define
     return of(confirm(message));
   }
 }
