1f1d1b81eb59c401353b69eeb0b7fabae812de56
coma64 <zaton.tristan@gmail.com>
2023-05-03 15:41:43 +0200

    Add bahn api endpoints

--- backend/handlers/bahn.go
+++ backend/handlers/bahn.go
@@ -1,14 +1,64 @@
 package handlers
 
 import (
+	"github.com/coma64/bahn-alarm-backend/external_apis/bahn"
 	"github.com/coma64/bahn-alarm-backend/server"
 	"github.com/labstack/echo/v4"
+	"net/http"
+	"time"
 )
 
+func bahnConnectionToSchema(conn *bahn.Trip) server.BahnConnection {
+	return server.BahnConnection{
+		Departure: struct {
+			ScheduledTime time.Time `json:"scheduledTime"`
+		}{
+			ScheduledTime: conn.Departure.ScheduledTime,
+		},
+	}
+}
+
 func (b *BahnAlarmApi) GetBahnConnections(ctx echo.Context, params server.GetBahnConnectionsParams) error {
-	return nil
+	connections, err := bahn.FetchConnections(ctx.Request().Context(), params.Departure, params.FromId, params.ToId)
+	if err != nil {
+		return &echo.HTTPError{
+			Code:     http.StatusServiceUnavailable,
+			Message:  "cannot reach bahn API",
+			Internal: err,
+		}
+	}
+
+	schemas := make([]server.BahnConnection, 0, len(connections.Trips))
+	for _, conn := range connections.Trips {
+		schemas = append(schemas, bahnConnectionToSchema(&conn))
+	}
+
+	return ctx.JSON(http.StatusOK, server.BahnConnectionsList{Connections: schemas})
+}
+
+func bahnPlaceToSchema(place *bahn.Place) server.BahnPlace {
+	return server.BahnPlace{
+		Id:        place.ID,
+		Label:     place.Label,
+		Name:      place.Name,
+		StationId: place.StationID,
+	}
 }
 
 func (b *BahnAlarmApi) GetBahnPlaces(ctx echo.Context, params server.GetBahnPlacesParams) error {
-	return nil
+	places, err := bahn.FetchPlaces(ctx.Request().Context(), params.Name)
+	if err != nil {
+		return &echo.HTTPError{
+			Code:     http.StatusServiceUnavailable,
+			Message:  "cannot reach bahn API",
+			Internal: err,
+		}
+	}
+
+	schemas := make([]server.BahnPlace, 0, len(places.Places))
+	for _, place := range places.Places {
+		schemas = append(schemas, bahnPlaceToSchema(&place))
+	}
+
+	return ctx.JSON(http.StatusOK, server.BahnPlacesList{Places: schemas})
 }
