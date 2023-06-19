1b3ef37aa698f8b48be62df4a751e0f0a4207afb
coma64 <zaton.tristan@gmail.com>
2023-06-19 21:50:51 +0200

    Always deploy latest version

--- frontend/scripts/build-docker-image.bash
+++ frontend/scripts/build-docker-image.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-[[ -z $1 ]] && echo 'you need to specify a version e.g. "pnpm run docker v10"' && exit 69
-
-docker buildx build --platform linux/amd64 --push --tag "registry.zat.ong/bahn-alarm-frontend:$1" .
+docker buildx build \
+  --platform linux/amd64 \
+  --push \
+  --tag "registry.zat.ong/bahn-alarm-frontend:$(git describe --tags)" \
+  --tag "registry.zat.ong/bahn-alarm-frontend:latest" \
+  .
