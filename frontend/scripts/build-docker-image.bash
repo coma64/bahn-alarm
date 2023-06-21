bb23003fc879488f352d716e30db3b0907917ac2
coma64 <zaton.tristan@gmail.com>
2023-06-21 22:16:50 +0200

    Remove v prefix from docker image version

--- frontend/scripts/build-docker-image.bash
+++ frontend/scripts/build-docker-image.bash
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+VERSION=$(git describe --tags | cut -c 2-)
+echo "${VERSION}" | figlet
+
 docker buildx build \
   --platform linux/amd64 \
   --push \
-  --tag "registry.zat.ong/bahn-alarm-frontend:$(git describe --tags)" \
+  --tag "registry.zat.ong/bahn-alarm-frontend:${VERSION}" \
   --tag "registry.zat.ong/bahn-alarm-frontend:latest" \
   .
