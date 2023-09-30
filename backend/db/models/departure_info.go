078f2109aa9bdaefcf296b4ffc8b798c1ff12e26
coma64 <zaton.tristan@gmail.com>
2023-09-30 09:36:24 +0200

    Add logging to the DepartureStatus() function

--- backend/db/models/departure_info.go
+++ backend/db/models/departure_info.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"github.com/coma64/bahn-alarm-backend/server"
+	"github.com/rs/zerolog/log"
 	"time"
 )
 
@@ -13,13 +14,25 @@ type DepartureInfo struct {
 }
 
 func (d *DepartureInfo) DepartureStatus() server.TrackedDepartureStatus {
+	logEvent := log.Debug().
+		Int("departureId", d.DepartureId).
+		Time("scheduledTime", d.ScheduledTime)
+
+	if d.ActualTime != nil {
+		logEvent = logEvent.Time("actualTime", *d.ActualTime)
+	}
+
 	if d == nil {
+		logEvent.Msg("DepartureInfo is nil. Setting status to NotChecked")
 		return server.NotChecked
 	} else if d.ActualTime == nil {
+		logEvent.Msg("ActualTime is nil. Settings status to Canceled")
 		return server.Canceled
 	} else if d.ScheduledTime.Equal(*d.ActualTime) {
+		logEvent.Msg("ScheduledTime is the same as ActualTime. Setting status to OnTime")
 		return server.OnTime
 	} else {
+		logEvent.Msg("Else case. Setting status to Delayed")
 		return server.Delayed
 	}
 }
