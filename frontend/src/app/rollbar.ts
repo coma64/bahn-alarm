55c73958dd5c2b51b8a14ddd7c779241de684c54
coma64 <zaton.tristan@gmail.com>
2023-10-03 13:17:27 +0200

    Disable rollbar and logrocket locally

--- frontend/src/app/rollbar.ts
+++ frontend/src/app/rollbar.ts
@@ -1,12 +1,13 @@
-import Rollbar, {Configuration} from 'rollbar';
+import Rollbar, { Configuration } from 'rollbar';
 
 import {
   Injectable,
   Inject,
   InjectionToken,
-  ErrorHandler
+  ErrorHandler,
+  isDevMode,
 } from '@angular/core';
-import LogRocket from "logrocket";
+import LogRocket from 'logrocket';
 
 const rollbarConfig: Configuration = {
   accessToken: '771442c822394e9787f6fc2dfcaa645a',
@@ -20,10 +21,15 @@ export const rollbarService = new InjectionToken<Rollbar>('rollbar');
 export class RollbarErrorHandler implements ErrorHandler {
   constructor(@Inject(rollbarService) private readonly rollbar: Rollbar) {}
 
-  handleError(err:any) : void {
-    this.rollbar.error(err.originalError || err);
-    LogRocket.captureException(err);
+  handleError(err: any): void {
     console.error(err);
+
+    if (isDevMode()) {
+      return;
+    }
+
+    this.rollbar.error(err.originalError ?? err);
+    LogRocket.captureException(err);
   }
 }
 
